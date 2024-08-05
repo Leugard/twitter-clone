18ad69efbcbb5e8039fb7f39bffcb0cc2ecc41bc
Leugard <kirakazama01@gmail.com>
2024-08-05 19:14:48 +0700

    chore: Update project configuration files and add provider dependency

---
+++ lib/themes/theme_provider.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:twitter_clone/themes/dark_mode.dart';
+import 'package:twitter_clone/themes/light_mode.dart';
+
+class ThemeProvider with ChangeNotifier {
+  // Initially, set is as light mode
+  ThemeData _themeData = lightMode;
+
+  // Get current theme
+  ThemeData get themeData => _themeData;
+
+  // is it dark mode currently?
+  bool get isDarkMode => _themeData == darkMode;
+
+  // set the theme
+  set themeData(ThemeData themeData) {
+    _themeData = themeData;
+
+    // Update UI
+    notifyListeners();
+  }
+
+  // Toggle between dark & light mode
+  void toggleMode() {
+    if (_themeData == lightMode) {
+      themeData = darkMode;
+    } else {
+      themeData = lightMode;
+    }
+  }
+}
